a2d5bd5ec99c92f120946dc5753cf92539e3f8e7
max-wp <max-wp@mail.ru>
2023-04-14 15:30:32 +0300

    Первая выгрузка

---
+++ Disable-UserAD.ps1
@@ -0,0 +1,70 @@
+﻿#Функция отключает учетную запись домена и переносит её в контейнер "уволившиеся сотрудники" с простановкой даты увольнения в описании (отключения)
+#Поиск происходит по Фамилии, Имени, Отчеству, табельному номеру или логину. Если пользователь не один, нужно скопировать табельный номер или логин уволившегося сотруднника и вызвать функцию повторно вставив в область поиска новые данные.
+
+function Disable-MOHPUser([parameter (Mandatory=$true, HelpMessage='Введите Фамилию, логин или табельный номер пользователя')][string]$ADNameUser){
+    #Область поиска
+    $search_base='DC=mohp,DC=ru'
+    $search_ADName = "*$ADNameUser*"
+    $dateStr = Get-Date -Format "yyyy-MM-dd"
+    
+    #Группа для переноса
+    $targetOU = 'OU=уволившиеся сотрудники,DC=MOHP,DC=RU'
+
+    $MUser = Get-ADUser -filter "(Name -like '$search_ADName') -or (extensionAttribute1 -like '$search_ADName') -or (SamAccountName -like '$search_ADName')" -SearchBase "$search_base" -Properties * | Select-Object Name, Enabled, extensionAttribute1, Department, title, SamAccountName, distinguishedName, mailNickname, description
+    
+    if (-not $MUser){
+    
+        Write-Host 'Пользователь не найден' -ForegroundColor 'Red'
+        exit
+    }
+    
+    $userCount = $MUser.count
+    if ($userCount -ge 1){
+        
+        Write-Host "`nНайдено $userCount сотрудников:" -ForegroundColor 'Green'
+        $Muser
+        Write-Host "Для точной идентификации введите табельный номер или логин отключаемого пользователя" -ForegroundColor 'Red'
+        exit
+
+    }
+    
+    #Отключаем учетную запись
+    Disable-ADAccount -Identity $MUser.SamAccountName# -Confirm
+    
+    #Переносим объект учетной записи в новый контейнер - Уволившиеся сотрудники
+    Move-ADObject -Identity $MUser.distinguishedName -TargetPath $targetOU
+    Set-ADUser $MUser.SamAccountName -Add @{description="Уволен $dateStr"}
+
+    #Почта
+
+    function Connect-mailServer{
+        param($nikMailHide)
+        function Send-MailMess {
+            Send-MailMessage -SmtpServer HD-MAIL -To 'TulpakovMS@hydroproject.com, Korneevvv@hydroproject.com' -From 'admin@hydroproject.com' -Subject "$nikMailHide уволен" -Body "Пользователь $nikMailHide уволен. Сообщение создано автоматически, отвечать на него не нужно!" -Encoding 'UTF8'
+        }
+
+        #Подключиться к удаленному серверу Exchange
+        $UserCredential = Get-Credential
+    
+        $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://hd-mail.mohp.ru/PowerShell/ -Authentication Kerberos -Credential $UserCredential
+    
+        Import-PSSession $Session -DisableNameChecking
+        
+        #Скрываем ящик на сервере
+        Set-Mailbox $nikMailHide -HiddenFromAddressListsEnabled $True
+
+        #Обязательное завершение сессии
+        Remove-PSSession $Session
+
+        Send-MailMess
+
+    }
+    Connect-mailServer $MUser.mailNickname
+
+    #Диагностическое сообщение об успешности операции
+    Write-Host 'Учетная запись отключена' -ForegroundColor Green
+    $null = $MUser
+    $MUser = Get-ADUser -filter "(Name -like '$search_ADName') -or (extensionAttribute1 -like '$search_ADName') -or (SamAccountName -like '$search_ADName')" -SearchBase "$search_base" -Properties * | Select-Object Name, Enabled, description, distinguishedName
+    $MUser
+    
+}
